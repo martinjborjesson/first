95a7f6ac73b28a8e14361e2630401de4871d05fd
Martin B <martinjborjesson@gmail.com>
2025-09-26 18:04:02 +0200

    remove log

--- providers/theme-provider.tsx
+++ providers/theme-provider.tsx
@@ -6,7 +6,6 @@ export const ThemeContext = createContext<Theme>(themesList.dark);
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const scheme = useColorScheme();
-  console.log("Color scheme: ", scheme)
   const value = scheme === "dark" ? themesList.dark : themesList.light;
 
   return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>;
