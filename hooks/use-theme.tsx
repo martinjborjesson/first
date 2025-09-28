7410d61279eed2c6c39a7c728d94d22f7dc9a902
Martin B <martinjborjesson@gmail.com>
2025-09-28 18:55:41 +0200

    add hooks for theme usage

--- hooks/use-theme.tsx
+++ hooks/use-theme.tsx
@@ -1,6 +1,14 @@
 import { ThemeContext } from "@/providers/theme-provider";
 import { useContext } from "react";
 
-export default function UseTheme() {
-  return useContext(ThemeContext);
+export function useTheme() {
+  return useContext(ThemeContext).currentTheme;
+}
+
+export function useSetTheme() {
+  return useContext(ThemeContext).setTheme;
+}
+
+export function useToggleTheme() {
+  return useContext(ThemeContext).toggleTheme;
 }
