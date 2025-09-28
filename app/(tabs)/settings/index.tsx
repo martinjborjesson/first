66b076bad1c6e14103fd58703c53338c47ca0d96
Martin B <martinjborjesson@gmail.com>
2025-09-28 13:42:54 +0200

    add settings screen

---
+++ app/(tabs)/settings/index.tsx
@@ -0,0 +1,30 @@
+import SafeAreaView from "@/components/safe-area-view";
+import Text from "@/components/text";
+import View from "@/components/view";
+import { ScrollView, StyleSheet } from "react-native";
+
+export default function PlacesScreen() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={s.container}>
+          <Text style={s.bigText}>
+            SETTINGS
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bigText: {
+    fontSize: 30,
+    marginTop: 15,
+  },
+})
