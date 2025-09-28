6519a43c96f172ac9daf7a9455eb01d59dfe42f2
Martin B <martinjborjesson@gmail.com>
2025-09-28 13:05:05 +0200

    add replacement for SafeAreaView

---
+++ components/safe-area-view.tsx
@@ -0,0 +1,13 @@
+import UseTheme from "@/hooks/use-theme";
+import { ComponentProps } from "react";
+import { SafeAreaView as SafeAreaViewRN } from "react-native-safe-area-context";
+
+type Props = ComponentProps<typeof SafeAreaViewRN>;
+
+export default function SafeAreaView({ style, ...rest }: Props) {
+  const theme = UseTheme();
+
+  return (
+    <SafeAreaViewRN {...rest} style={[{ flex: 1, backgroundColor: theme.background }, style,]} />
+  );
+}
