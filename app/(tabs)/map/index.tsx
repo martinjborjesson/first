fd506c4c254c0b20b671d3078373d714b75925a9
Martin B <martinjborjesson@gmail.com>
2025-09-30 14:16:20 +0200

    add MapView to map page

--- app/(tabs)/map/index.tsx
+++ app/(tabs)/map/index.tsx
@@ -1,18 +1,53 @@
 import SafeAreaView from "@/components/safe-area-view";
-import Text from "@/components/text";
 import View from "@/components/view";
-import { ScrollView, StyleSheet } from "react-native";
+import { mockData } from "@/data";
+import { StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function MapScreen() {
+  const markers = mockData.filter(place => place.id !== "patrik").map(({ id, name, description, coordinates }) => ({
+    id,
+    name,
+    description,
+    coordinates,
+  }));
+
   return (
     <SafeAreaView>
-      <ScrollView>
-        <View style={s.container}>
-          <Text style={s.bigText}>
-            MAP
-          </Text>
-        </View>
-      </ScrollView>
+      <View style={s.container}>
+        <MapView
+          style={s.map}
+          initialRegion={{
+            latitude: 59.6,
+            longitude: 14.4,
+            latitudeDelta: 8.0,
+            longitudeDelta: 2.0,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: 57.714186790674404,
+              longitude: 12.796773591829576,
+            }}
+            title="Patrik Lax"
+            description="Kung."
+            image={require("@/assets/app/patrik.png")}
+          />
+          {markers.map((m) => {
+            return (
+              <Marker
+                key={m.id}
+                coordinate={{
+                  latitude: m.coordinates.latitude,
+                  longitude: m.coordinates.longitude,
+                }}
+                title={m.name}
+                description={m.description}
+              />
+            );
+          })}
+        </MapView>
+      </View>
     </SafeAreaView>
   );
 }
@@ -20,11 +55,9 @@ export default function MapScreen() {
 const s = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
-  bigText: {
-    fontSize: 30,
-    marginTop: 15,
+  map: {
+    width: '100%',
+    height: '100%',
   },
 })
