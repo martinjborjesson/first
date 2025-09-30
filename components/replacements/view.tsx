ac92865c13db466916010bd3e515f8e41cb03686
Martin B <martinjborjesson@gmail.com>
2025-09-30 17:27:21 +0200

    add background boolean and logic to select background color

--- components/replacements/view.tsx
+++ components/replacements/view.tsx
@@ -4,13 +4,21 @@ import { View as ViewRN } from "react-native";
 
 type Props = ComponentProps<typeof ViewRN> & {
   surface?: boolean;
+  background?: boolean;
 };
 
-export default function View({ style, children, surface, ...rest }: Props) {
+export default function View({ style, children, surface, background, ...rest }: Props) {
   const theme = useTheme();
+  let backgroundColor = "unset";
+
+  if (background) {
+    backgroundColor = theme.background;
+  } else if (surface) {
+    backgroundColor = theme.surface;
+  }
 
   return (
-    <ViewRN {...rest} style={[{ backgroundColor: surface ? theme.surface : theme.background }, style]}>
+    <ViewRN {...rest} style={[{ backgroundColor: backgroundColor }, style]}>
       {children}
     </ViewRN>
   );
