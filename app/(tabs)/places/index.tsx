fd92c9d2680197b2314dd6265067853f7e40e88e
Martin B <martinjborjesson@gmail.com>
2025-10-01 14:32:18 +0200

    implement shelterListItem

--- app/(tabs)/places/index.tsx
+++ app/(tabs)/places/index.tsx
@@ -1,16 +1,28 @@
+import BackgroundImage from "@/components/background-image";
 import SafeAreaView from "@/components/replacements/safe-area-view";
 import Text from "@/components/replacements/text";
 import View from "@/components/replacements/view";
+import ShelterListItem from "@/components/shelter-list-item";
+import { mockData } from "@/data";
+import { languages } from "@/languages";
 import { ScrollView, StyleSheet } from "react-native";
 
 export default function PlacesScreen() {
+  const language = languages.swedish;
+  const shelters = mockData;
+
   return (
     <SafeAreaView>
+      <BackgroundImage />
       <ScrollView>
         <View style={s.container}>
           <Text style={s.bigText}>
-            PLACES
+            {language.places.title}
           </Text>
+          {shelters.map((s) => {
+            return <ShelterListItem shelter={s} key={s.id} />
+          })}
+
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -20,10 +32,12 @@ export default function PlacesScreen() {
 const s = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    padding: 20,
   },
   bigText: {
-    fontSize: 30,
-    marginTop: 15,
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 16,
+    textAlign: "center",
   },
 })
